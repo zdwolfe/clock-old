e28d8bf3f1aa657809b254fc9eaf5c7707d81e74
Zach Wolfe <wolfe.zach@gmail.com>
2014-09-21 17:54:13 -0700

    remove website from arduino branch

--- app.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $('#casi').click(function() {
-    $('td.active').toggleClass('wasActive');
-    $('td.wasActive').toggleClass('active');
-    $('td.casi,td.wolfe').toggleClass('active');
-  });
-});
