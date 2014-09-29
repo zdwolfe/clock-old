42d1a2d63e89e7c7cb09c066f205cf74b37da3a5
Zach Wolfe <wolfe.zach@gmail.com>
2014-09-28 17:32:40 -0700

    add ids

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 $(function() {
-  $('#casi').click(function() {
-    $('td.active').toggleClass('wasActive');
-    $('td.wasActive').toggleClass('active');
-    $('td.casi,td.wolfe').toggleClass('active');
+  
+  var clock = {
+    hour: 0,
+    minute: 0,
+    second: 0
+  };
+
+  $('button#casi').mousedown(function() {
+    $('td.active').addClass('wasActive').removeClass('active');
+    $('td.casi, td.wolfe').addClass('active');
+  });
+
+  $('button#casi').mouseup(function() {
+    $('td.wasActive').removeClass('wasActive').addClass('active');
+    $('td.casi, td.wolfe').removeClass('active');
+  });
+
+  $('button#hour').click(function() {
+    clock.hour++;
+  });
+
+  $('button#minute').click(function() {
+  });
+
+  $('input#alarm').change(function() {
   });
 });
