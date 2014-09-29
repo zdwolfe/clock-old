a43487056375e63be64ef2da248d30b729ace407
Zach Wolfe <wolfe.zach@gmail.com>
2014-09-28 17:53:38 -0700

    script

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 $(function() {
-  
-  var clock = {
-    hour: 0,
-    minute: 0,
-    second: 0
+  var $minutes = $('.minute');
+  var $hours = $('.hour');
+
+  var minuteMap = {
+    5: $('.P_five, .P_past'),
+    10: $('.P_ten, .P_past'),
+    15: $('.P_quarter, .P_past'),
+    20: $('.P_twenty, .P_past'),
+    30: $('.P_half, .P_past'),
+    40: $('.P_twenty, .P_to'),
+    45: $('.P_quarter, .P_to'),
+    50: $('.P_ten, .P_to')
   };
 
-  $('button#casi').mousedown(function() {
+  function addMinute() {
+  }
+
+  function setMinute(minute) {
+    $minutes.removeClass('active');
+    $(minuteMap[minute]).addClass('active');
+  };
+
+  function setHour(hour) {
+    $hours.removeClass('active');
+    $(hourMap[hour]).addClass('active');
+  };
+
+  $('button#CW').mousedown(function() {
     $('td.active').addClass('wasActive').removeClass('active');
-    $('td.casi, td.wolfe').addClass('active');
+    $('td.CW').addClass('active');
   });
 
-  $('button#casi').mouseup(function() {
+  $('button#CW').mouseup(function() {
     $('td.wasActive').removeClass('wasActive').addClass('active');
-    $('td.casi, td.wolfe').removeClass('active');
+    $('td.CW').removeClass('active');
   });
 
-  $('button#hour').click(function() {
-    clock.hour++;
+  $('button#hour').click(addHour);
+
+  $('button#minute').click(addMinute);
+
+  $('input#toggleAlarm').change(function() {
   });
 
-  $('button#minute').click(function() {
-  });
-
-  $('input#alarm').change(function() {
+  var ONE_MINUTE = 1000*60;
+  setInterval(ONE_MINUTE, function() {
+    var now = new Date();
+    var minute = now.getMinutes();
+    if (minute % 5 === 0) {
+    }
   });
 });
