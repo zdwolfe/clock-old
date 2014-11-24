a84b53e9d9a891b2c36f71dc74bb5339df84aeff
Zach Wolfe <wolfe.zach@gmail.com>
2014-11-23 20:24:56 -0800

    only use 'oclock' when on the hour. added number of active cells

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 $(function() {
   var num_cells = $('td').length;
   var num_usable_cells = num_cells - $('td.extra').length;
-
   var num_rows = $('tr').length;
   var num_cols = $('tr').first().children('td').length;
+  var num_active_cells = $('td.active').length;
 
   $('#num_cells').text(num_cells);
+  $('#num_active_cells').text(num_active_cells);
   $('#num_usable_cells').text(num_usable_cells);
   $('#num_rows').text(num_rows);
   $('#num_cols').text(num_cols);
