ede768ca4309ba72ed6c0d88556cb132e0d729af
Zach Wolfe <wolfe.zach@gmail.com>
2014-11-23 16:38:41 -0800

    info

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 $(function() {
+
+  var num_cells = $('td').length;
+  var num_usable_cells = num_cells - $('td.extra').length;
+
+  var num_rows = $('tr').length;
+  var num_cols = $('tr').first().children('td').length;
+
+  $('#num_cells').text(num_cells);
+  $('#num_usable_cells').text(num_usable_cells);
+  $('#num_rows').text(num_rows);
+  $('#num_cols').text(num_cols);
 });
